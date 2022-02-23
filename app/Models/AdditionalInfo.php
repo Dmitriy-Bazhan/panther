d51d24fc9a03e834adee5f7c88658b407b2c97c2
Dmitriy Bazhan <dbazhan@webiprog.de>
2022-02-23 19:00:16 +0200

    temp

--- app/Models/AdditionalInfo.php
+++ app/Models/AdditionalInfo.php
@@ -9,6 +9,10 @@ class AdditionalInfo extends Model
 {
     use HasFactory;
 
+    protected $hidden = [
+        'laravel_through_key'
+    ];
+
     public function data() {
         return $this->hasOne('App\Models\AdditionalInfoData', 'additional_info_id', 'id')
             ->where('lang', auth()->user()->prefs->pref_lang);
