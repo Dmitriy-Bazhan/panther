23604c89841ccb348bbdb3ee875fd1cf35e76984
apolikanin <apolikanin@webiprog.de>
2022-04-15 17:11:19 +0300

    integrate admin data to home page

---
+++ resources/js/components/PageBlocks/BenefitsBlock.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="pt-section-benefits">
+        <div class="wrapper">
+            <p class="pt-subtitle">
+                <span>vorteile</span>
+            </p>
+            <h2 class="pt-title">
+                PflegePanther – wir stehen für freundliche und qualifizierte Pflege
+            </h2>
+
+            <div class="pt-row pt-section-benefits--items">
+                <div class="pt-col-md-6" v-for="n in 4">
+                    <div class="pt-section-benefits--item">
+                        <div class="pt-section-benefits--item-title">
+                            <div class="pt-section-benefits--item-icon">
+                                <pt-icon type="decor"></pt-icon>
+                            </div>
+                            Geprüfte Qualität
+                        </div>
+                        <div class="pt-section-benefits--item-text">
+                            Sämtliche der auf der Plattform registrierten Pflegekräfte wurden von unserem Team von Experten auf ihre nachweisbare Qualifikation hin geprüft
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "BenefitsBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/PageBlocks/InfoBlock.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="pt-section-register">
+        <div class="main-wrapper">
+            <div class="pt-section-register--items">
+                <div class="pt-section-register--item">
+                    <img src="/images/fake/nurse.png" alt="" class="pt-section-register--item-img">
+                    <p class="pt-block-heading">
+                        Sie sind Pflegekraft und möchten
+                        flexibel, selbstbestimmt arbeiten?
+                    </p>
+                    <p class="pt-section-register--item-text">
+                        Wir verbinden Sie mit denjenigen Pflegebedürftigen und Angehörigen, die Ihre Dienste brauchen. Dabei kümmern wir uns um Verträge, Versicherungsschutz Medikamentenpläne und Terminplanung: Sie können sich komplett auf die Pflege konzentrieren, ihren Lohn legen Sie im Vorfeld selbst fest
+                    </p>
+
+                    <a href="" class="pt-btn pt-lg">Registrieren</a>
+                </div>
+                <div class="pt-section-register--item">
+                    <img src="/images/fake/old.png" alt="" class="pt-section-register--item-img">
+                    <p class="pt-block-heading">
+                        Sie sind Angehörige/r einer
+                        pflegebedürftigen Person und suchen
+                        nach qualifizierter Pflege?
+                    </p>
+                    <p class="pt-section-register--item-text">
+                        Dann ist PflegePanther genau die richtige Wahl. Unter Berücksichtigung der gewünschten Qualifikation, Berufserfahrung, Sprachen, zeitlicher Verfügbarkeit und Vielem mehr suchen wir genau zu Ihnen passende Pflegekräfte in Ihrer Umgebung.
+                    </p>
+
+                    <a href="" class="pt-btn pt-lg">Registrieren</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "InfoBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
