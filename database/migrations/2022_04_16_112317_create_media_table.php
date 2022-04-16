36e64c83b40f0eddb8681c709fef7b26fdb2d75b
dbazhan <webiprog@dbazhan.de>
2022-04-16 15:38:48 +0300

    make media table

---
+++ database/migrations/2022_04_16_112317_create_media_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('file_name');
+            $table->string('type');
+            $table->string('extension');
+            $table->string('media_type')->comment('pages_image|others');
+            $table->float('size');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
