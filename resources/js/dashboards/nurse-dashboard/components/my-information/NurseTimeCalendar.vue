712b12f108bc9ad62488e26cfe445729fd5a0fe0
dbazhan <webiprog@dbazhan.de>
2022-05-12 16:32:16 +0300

    work on time

--- resources/js/dashboards/nurse-dashboard/components/my-information/NurseTimeCalendar.vue
+++ resources/js/dashboards/nurse-dashboard/components/my-information/NurseTimeCalendar.vue
@@ -59,71 +59,28 @@
         </div>
 
         <div class="row">
-            <div class="col-8 offset-2">
-                <div class="row">
-                    <div class="col-4 offset-4 justify-content-center">{{ $t('weekdays') }}</div>
-                    <div class="col-4 justify-content-center">{{ $t('weekends') }}</div>
-                </div>
-                <div class="row">
-                    <div class="col-4 offset-4 justify-content-center">{{ $t('mon_fri') }}</div>
-                    <div class="col-4 justify-content-center">{{ $t('sat_sun') }}</div>
-                </div>
+            <div class="col-3 offset-3">
+                <h5>{{ $t('weekdays') }}</h5>
+                <div v-for="(item,index) in time_intervals">
+                    <template v-if="item.type ==='weekdays'">
+                        <h6>{{ item.interval }}</h6>
+                        <input type="checkbox" true-value="1" false-value="0"
+                               v-model="user.entity.work_time_pref[item.id]">
 
-                <!--                        7-11 Uhr  -->
-                <div class="row">
-                    <div class="col-4">7-11 Uhr</div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekdays_morning" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekdays_7_11">
-                        &nbsp;<label for="weekdays_morning"></label>
-                    </div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekends_morning" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekends_7_11">
-                        &nbsp;<label for="weekends_morning"></label>
-                    </div>
+                        <hr>
+                    </template>
                 </div>
-                <!--                        11-14 Uhr  -->
-                <div class="row">
-                    <div class="col-4">11-14 Uhr</div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekdays_afternoon" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekdays_11_14">
-                        &nbsp;<label for="weekdays_afternoon"></label>
-                    </div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekends_afternoon" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekends_11_14">
-                        &nbsp;<label for="weekends_afternoon"></label>
-                    </div>
-                </div>
-                <!--                        14-17 Uhr -->
-                <div class="row">
-                    <div class="col-4">14-17 Uhr</div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekdays_evening" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekdays_14_17">
-                        &nbsp;<label for="weekdays_evening"></label>
-                    </div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekends_evening" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekends_14_17">
-                        &nbsp;<label for="weekends_evening"></label>
-                    </div>
-                </div>
-                <!--                        17-21 Uhr -->
-                <div class="row">
-                    <div class="col-4">17-21 Uhr</div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekdays_overnight" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekdays_17_21">
-                        &nbsp;<label for="weekdays_overnight"></label>
-                    </div>
-                    <div class="col-4 justify-content-center">
-                        <input type="checkbox" id="weekends_overnight" true-value="1" false-value="0"
-                               v-model="user.entity.work_time_pref.weekends_17_21">
-                        &nbsp;<label for="weekends_overnight"></label>
-                    </div>
+            </div>
+            <div class="col-3">
+                <h5>{{ $t('weekends') }}</h5>
+                <div v-for="(item,index) in time_intervals">
+                    <template v-if="item.type ==='weekends'">
+                        <h6>{{ item.interval }}</h6>
+                        <input type="checkbox" true-value="1" false-value="0"
+                               v-model="user.entity.work_time_pref[item.id]">
+
+                        <hr>
+                    </template>
                 </div>
             </div>
         </div>
@@ -143,12 +100,12 @@
         props: ['user', 'data', 'errors'],
         data() {
             return {
-
+                time_intervals: [],
             }
         },
         mounted() {
             console.log(this.user);
-
+            this.time_intervals = this.data['time_intervals'];
         },
         watch: {
             user: {
