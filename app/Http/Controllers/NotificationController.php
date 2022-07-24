7cd07d4a05f900db775ba3a7390927f3a383878c
Dmitriy-Bazhan <dmitriybazhan79@gmail.com>
2022-07-24 22:33:16 +0300

    temp

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\NotificationResource;
 use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class NotificationController extends Controller
 {
@@ -30,6 +31,16 @@ class NotificationController extends Controller
 
     static public function createNotification($target_user_id = null, $type = null, $content = null)
     {
+        if(is_null($target_user_id) || is_null($type) || is_null($content)){
+            Log::channel('app_logs')->error('NotificationController@createNotification Vars is null');
+            return false;
+        }
+
+        if(!User::find($target_user_id)){
+            Log::channel('app_logs')->error('NotificationController@createNotification Target user not exists');
+            return false;
+        }
+
         $notification = Notification::create([
             'creator_user_id' => auth()->id(),
             'creator_entity' => auth()->user()->entity,
@@ -44,6 +55,7 @@ class NotificationController extends Controller
             broadcast(new NewNotificationEvent($unread_notifications_count, $target_user_id));
             $success = true;
         }else{
+            Log::channel('app_logs')->error('NotificationController@createNotification Notification not created');
             $success = false;
         }
 
